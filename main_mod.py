cf5f2f499473a8c0dd956431a4386e57cf6e2c23
Ctrl Cmd <unalarisa@yahoo.com>
2020-10-29 22:55:30 +0100

    add automatic reload for all modules in the directory

--- main_mod.py
+++ main_mod.py
@@ -1,12 +1,6 @@
-import logging
+from log_mod import logger
 
-logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("my_log.log")
-logger.addHandler(file_handler)
-logger.setLevel(logging.INFO)
-stream_handler = logging.StreamHandler()
-logger.addHandler(stream_handler)
 
 print("main program is running")
-logger.debug('DEBUG')
-logger.info('INFO')
+logger.debug("DEBUG")
+logger.info("INFO")
--- watchdog_mod.py
+++ watchdog_mod.py
@@ -1,12 +1,29 @@
 from importlib import import_module
 from importlib import reload
+import os
+import sys
 
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 import time
-import main_mod
 
-patterns = "*"
+
+dirs = os.listdir()
+imported_modules = []
+current_module = __file__
+
+for dir in dirs:
+    dir_name = str(dir)
+    if dir_name.endswith(".py"):
+        module_name = dir_name[:-3]
+        if module_name == current_module[:-3]:
+            pass
+        else:
+            imported_module = import_module(module_name)
+            globals()[module_name] = imported_module
+            imported_modules.append(imported_module)
+
+patterns = ["*/log_mod.py"]
 ignore_patterns = ""
 ignore_directories = False
 case_sensitive = True
@@ -16,9 +33,11 @@ my_event_handler = PatternMatchingEventHandler(
 
 
 def on_modified(event):
-    global main_mod
     print(f"{event.src_path} has been modified")
-    main_mod = reload(main_mod)
+    for mod in imported_modules:
+        mod = reload(mod)
+        print(f"{mod.__name__} has been reloaded")
+
 
 my_event_handler.on_modified = on_modified
 
